7cfcc64308892b88c320a0e748426852912390b0
Martin Thoma <info@martin-thoma.de>
2018-05-04 23:21:09 +0200

    ENH: Add Money and currency class
    Resolves #1

---
+++ mpu/units/__init__.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Handle units - currently only currencies."""
+
+# core modules
+from dateutil import parser as dateparser
+import csv
+import fractions
+import pkg_resources
+
+
+class Money(object):
+    """Unit of account."""
+
+    def __init__(self, value, currency):
+        # Handle value
+        if isinstance(value, tuple):
+            if len(value) != 2:
+                raise ValueError(('value was {}, but only tuples of length 2 '
+                                  'str, int and decimal are allowed.')
+                                 .format(value))
+            self.value = fractions.Fraction(value[0], value[1])
+        elif isinstance(value, float):
+            raise ValueError('floats can be ambiguous. Please convert it to '
+                             'two integers (nominator and denominator) and '
+                             'pass a tuple to the constructor.')
+        else:
+            self.value = fractions.Fraction(value)  # convert to Decimal
+
+        # Handle currency
+        if isinstance(currency, Currency):
+            self.currency = currency
+        elif isinstance(currency, str):
+            self.currency = get_currency(currency)
+        else:
+            raise ValueError('currency is of type={}, but should be str or '
+                             'Currency')
+
+    def __str__(self):
+        exponent = 2
+        if self.currency.exponent is not None:
+            exponent = self.currency.exponent
+        return ('{value:0.{exponent}f} {currency}'
+                .format(exponent=exponent,
+                        value=float(self.value),
+                        currency=self.currency))
+
+    def __repr__(self):
+        return str(self)
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Money(self.value * other, self.currency)
+        else:
+            raise ValueError(('Multiplication with type \'{}\' is not '
+                              'supported').format())
+
+    def __add__(self, other):
+        if isinstance(other, Money):
+            if self.currency == other.currency:
+                return Money(self.value + other.value, self.currency)
+            else:
+                raise ValueError(('Addition of currency \'{}\' and \'{}\' is '
+                                  'not supported. You need an exchange rate.')
+                                 .format(self.currency, other.currency))
+        else:
+            raise ValueError(('Addition with type \'{}\' is not '
+                              'supported').format())
+
+    def __sub__(self, other):
+        if isinstance(other, Money):
+            if self.currency == other.currency:
+                return Money(self.value - other.value, self.currency)
+            else:
+                raise ValueError(('Subtraction of currency \'{}\' and \'{}\' '
+                                  'is not supported. You need an exchange '
+                                  'rate.')
+                                 .format(self.currency, other.currency))
+        else:
+            raise ValueError(('Subtraction with type \'{}\' is not '
+                              'supported').format())
+
+    def __truediv__(self, other):
+        if isinstance(other, Money):
+            if self.currency == other.currency:
+                return self.value / other.value
+            else:
+                raise ValueError(('Division of currency \'{}\' and \'{}\' '
+                                  'is not supported. You need an exchange '
+                                  'rate.')
+                                 .format(self.currency, other.currency))
+        elif isinstance(other, (int, fractions.Fraction)):
+            return Money(self.value / other, self.currency)
+        else:
+            raise ValueError(('Division with type \'{}\' is not '
+                              'supported').format())
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            same_currency = (self.currency == other.currency)
+            return (self.value == other.value) and same_currency
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    __rmul__ = __mul__
+
+
+def get_currency(currency_str):
+    """
+    Convert an identifier for a currency into a currency object.
+
+    Parameters
+    ----------
+    currency_str : str
+
+    Returns
+    -------
+    currency : Currency
+    """
+    path = 'units/currencies.csv'  # always use slash in Python packages
+    filepath = pkg_resources.resource_filename('mpu', path)
+    with open(filepath, 'r') as fp:
+        reader = csv.reader(fp, delimiter=',', quotechar='"')
+        next(reader, None)  # skip the headers
+        for row in reader:
+            is_currency = (row[0] == currency_str or row[1] == currency_str or
+                           row[2] == currency_str)
+            if is_currency:
+                entity = row[0]
+                name = row[1]
+                code = row[2]
+                numeric_code = row[3]
+                symbol = row[4]
+                if len(row[5]) == 0:
+                    exponent = None
+                else:
+                    exponent = int(row[5])
+                if len(row[6]) > 0:
+                    withdrawal_date = dateparser.parse(row[6])
+                else:
+                    withdrawal_date = None
+                subunits = row[7]
+                return Currency(name=name,
+                                code=code,
+                                numeric_code=numeric_code,
+                                symbol=symbol,
+                                exponent=exponent,
+                                entities=[entity],
+                                withdrawal_date=withdrawal_date,
+                                subunits=subunits)
+    raise ValueError('Could not find currency \'{}\''.format(currency_str))
+
+
+class Currency(object):
+    """Currency base class which contains information similar to ISO 4217."""
+
+    def __init__(self,
+                 name,
+                 code,
+                 numeric_code,
+                 symbol,
+                 exponent,
+                 entities,
+                 withdrawal_date,
+                 subunits):
+        if not isinstance(name, str):
+            raise ValueError('A currencies name has to be of type str, but '
+                             'was: {}'.format(type(name)))
+        if not isinstance(code, str):
+            raise ValueError('A currencies code has to be of type str, but '
+                             'was: {}'.format(type(code)))
+        if not isinstance(exponent, (type(None), int)):
+            raise ValueError('A currencies exponent has to be of type None '
+                             'or int, but was: {}'.format(type(code)))
+
+        self.name = name
+        self.code = code
+        self.numeric_code = numeric_code
+        self.symbol = symbol
+        self.exponent = exponent
+        self.entities = entities
+        self.withdrawal_date = withdrawal_date
+        self.subunits = subunits
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.numeric_code == other.numeric_code
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return str(self)
---
+++ tests/test_units.py
@@ -0,0 +1,114 @@
+# core modules
+import unittest
+
+# internal modules
+from mpu.units import Money, Currency
+
+
+class MoneyTests(unittest.TestCase):
+
+    def test_get_currency(self):
+        a = Money('0.1', 'EUR')
+        self.assertEquals(str(a), '0.10 Euro')
+        b = Money('0.1', 'USD')
+        self.assertEquals(str(b), '0.10 US Dollar')
+        with self.assertRaises(Exception):
+            Money('0.1', 'foobar')
+        c = Money((1, 100), 'EUR')
+        d = Money(5, 'ESP')
+        self.assertEquals(str(c), '0.01 Euro')
+        self.assertEquals(repr(c), '0.01 Euro')
+        self.assertEquals(str(d), '5.00 Spanish Peseta')
+        with self.assertRaises(Exception):
+            Money((5, 100, 42), 'EUR')
+        with self.assertRaises(Exception):
+            Money(0.1, 'EUR')
+        with self.assertRaises(Exception):
+            Money('0.1', None)
+
+    def test_currency_operations(self):
+        a = Money('0.5', 'EUR')
+        b = Money('0.1', 'EUR')
+        c = Money('0.1', 'USD')
+        d = Money('0.5', 'EUR')
+        self.assertEquals(a == b, False)
+        self.assertEquals(a == 0.5, False)
+        self.assertEquals(a == d, True)
+        self.assertEquals(a == c, False)
+        self.assertEquals(a != b, True)
+        self.assertEquals(a != d, False)
+        self.assertEquals(a != c, True)
+        self.assertEquals(str(a - b), '0.40 Euro')
+        with self.assertRaises(Exception):
+            a - c
+        with self.assertRaises(Exception):
+            a - 2
+        with self.assertRaises(Exception):
+            a - 2.0
+        self.assertEquals(str(a + b), '0.60 Euro')
+        with self.assertRaises(Exception):
+            a + c
+        with self.assertRaises(Exception):
+            a + 2
+        with self.assertRaises(Exception):
+            a + 2.0
+        self.assertEquals(str(2 * a), '1.00 Euro')
+        self.assertEquals(str(a / b), '5')
+        with self.assertRaises(Exception):
+            a / c
+        with self.assertRaises(Exception):
+            a * 3.141
+        with self.assertRaises(Exception):
+            3.141 * a
+        with self.assertRaises(Exception):
+            a / '0.1'
+        self.assertEquals(str(a / 2), '0.25 Euro')
+
+    def test_currency(self):
+        eur = Currency(name='Euro',
+                       code='EUR',
+                       numeric_code=123,
+                       symbol='€',
+                       exponent=2,
+                       entities=['Germany'],
+                       withdrawal_date=None,
+                       subunits=2)
+        usd = Currency(name='US Dollar',
+                       code='USD',
+                       numeric_code=456,
+                       symbol='$',
+                       exponent=2,
+                       entities=['United States of America'],
+                       withdrawal_date=None,
+                       subunits=2)
+        repr(eur)
+        self.assertEquals(eur == usd, False)
+        self.assertEquals(eur == 2, False)
+        self.assertEquals(eur != usd, True)
+        with self.assertRaises(Exception):
+            Currency(name=2,
+                     code='EUR',
+                     numeric_code=123,
+                     symbol='€',
+                     exponent=2,
+                     entities=['Germany'],
+                     withdrawal_date=None,
+                     subunits=2)
+        with self.assertRaises(Exception):
+            Currency(name='Euro',
+                     code=2,
+                     numeric_code=123,
+                     symbol='€',
+                     exponent=2,
+                     entities=['Germany'],
+                     withdrawal_date=None,
+                     subunits=2)
+        with self.assertRaises(Exception):
+            Currency(name='Euro',
+                     code='EUR',
+                     numeric_code=123,
+                     symbol='€',
+                     exponent='2',
+                     entities=['Germany'],
+                     withdrawal_date=None,
+                     subunits=2)
