bcb5adfa61ac88dcb6fc7985eb639711b7aad86b
Martin Thoma <info@martin-thoma.de>
2018-05-06 22:44:53 +0200

    MAINT: Set html_domain_indices to True

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -166,7 +166,7 @@ html_static_path = ['_static']
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+html_domain_indices = True
 
 # If false, no index is generated.
 #html_use_index = True
