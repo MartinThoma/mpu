75cdbb38587f98946132162a55967a073bf59ff7
Martin Thoma <info@martin-thoma.de>
2018-05-15 23:04:58 +0200

    ENH: Add io.write

--- mpu/io.py
+++ mpu/io.py
@@ -3,10 +3,18 @@
 
 """Reading and writing common file formats."""
 
+from __future__ import absolute_import
+
 # core modules
 import csv
 import json
 import os
+# Make it work for Python 2+3 and with Unicode
+import io as io_stl
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
 
 # internal modules
 from mpu.datastructures import EList
@@ -24,7 +32,8 @@ def read(filepath, **kwargs):
     Parameters
     ----------
     filepath : str
-        Path to the file that should be read.
+        Path to the file that should be read. This methods action depends
+        mainly on the file extension.
     kwargs : dict
         Any keywords for the specific file format.
 
@@ -42,16 +51,65 @@ def read(filepath, **kwargs):
                 kwargs['skiprows'] = []
             if isinstance(kwargs['skiprows'], int):
                 kwargs['skiprows'] = [i for i in range(kwargs['skiprows'])]
-            reader = csv.reader(fp,
-                                delimiter=kwargs['delimiter'],
-                                quotechar=kwargs['quotechar'])
+            skiprows = kwargs['skiprows']
+            kwargs.pop('skiprows', None)
+            reader = csv.reader(fp, **kwargs)
             data = EList([row for row in reader])
-            data = data.remove_indices(kwargs['skiprows'])
+            data = data.remove_indices(skiprows)
         return data
     elif filepath.lower().endswith('.json'):
         with open(filepath) as data_file:
-            data = json.load(data_file)
+            data = json.load(data_file, **kwargs)
         return data
+    else:
+        raise NotImplementedError('File \'{}\' is not known.'.format(filepath))
+
+
+def write(filepath, data, **kwargs):
+    """
+    Write a file.
+
+    Supported formats:
+
+    * CSV
+    * JSON
+
+    Parameters
+    ----------
+    filepath : str
+        Path to the file that should be read. This methods action depends
+        mainly on the file extension.
+    data : dict or list
+        Content that should be written
+    kwargs : dict
+        Any keywords for the specific file format.
+
+    Returns
+    -------
+    data : str or bytes
+    """
+    if filepath.lower().endswith('.csv'):
+        with open(filepath, 'r') as fp:
+            if 'delimiter' not in kwargs:
+                kwargs['delimiter'] = ','
+            if 'quotechar' not in kwargs:
+                kwargs['quotechar'] = '"'
+            with open(filepath, 'w') as fp:
+                writer = csv.writer(fp, **kwargs)
+                writer.writerows(data)
+        return data
+    elif filepath.lower().endswith('.json'):
+        with io_stl.open(filepath, 'w', encoding='utf8') as outfile:
+            if 'indent' not in kwargs:
+                kwargs['indent'] = 4
+            if 'sort_keys' not in kwargs:
+                kwargs['sort_keys'] = True
+            if 'separators' not in kwargs:
+                kwargs['separators'] = (',', ': ')
+            if 'ensure_ascii' not in kwargs:
+                kwargs['ensure_ascii'] = False
+            str_ = json.dumps(data, **kwargs)
+            outfile.write(to_unicode(str_))
     else:
         raise NotImplementedError('File \'{}\' is not known.'.format(filepath))
 
--- tests/test_io.py
+++ tests/test_io.py
@@ -8,7 +8,7 @@ import pkg_resources
 import unittest
 
 # internal modules
-from mpu.io import (download, read)
+from mpu.io import (download, read, write)
 
 
 class IoTest(unittest.TestCase):
@@ -32,7 +32,8 @@ class IoTest(unittest.TestCase):
                     ['42', " it says, ", '2.0'],
                     ['1337', "is about the most ", '-1'],
                     ['0', "massively useful thing ", '123'],
-                    ['-2', "an interstellar hitchhiker can have.", '3']]
+                    ['-2', "an interstellar hitchhiker can have.", '3'],
+                    ['3.141', "Special char test: €üößł", '2.7']]
         self.assertEquals(len(data_real), len(data_exp))
         self.assertEquals(data_real[0], data_exp[0])
         self.assertEquals(data_real, data_exp)
@@ -40,6 +41,18 @@ class IoTest(unittest.TestCase):
         self.assertEquals(data_real, data_exp[1:])
         data_real = read(source, skiprows=1, delimiter=',', quotechar='"')
         self.assertEquals(data_real, data_exp[1:])
+
+    def test_write_csv(self):
+        handle, filepath = mkstemp(suffix='.csv', prefix='mpu_test')
+        data = [['1', "A towel,", '1.0'],
+                ['42', " it says, ", '2.0'],
+                ['1337', "is about the most ", '-1'],
+                ['0', "massively useful thing ", '123'],
+                ['-2', "an interstellar hitchhiker can have.", '3']]
+        write(filepath, data)
+        data_read = read(filepath)
+        self.assertEquals(data, data_read)
+        os.remove(filepath)  # cleanup of mkstemp
 
     def test_read_hdf5(self):
         path = '../tests/files/example.hdf5'
@@ -58,6 +71,18 @@ class IoTest(unittest.TestCase):
                                      'key': 'value',
                                      'the answer': 42}}
         self.assertEquals(data_real, data_exp)
+
+    def test_write_json(self):
+        handle, filepath = mkstemp(suffix='.json', prefix='mpu_test')
+        data = {'a list': [1, 42, 3.141, 1337, 'help', u'€'],
+                'a string': 'bla',
+                'another dict': {'foo': 'bar',
+                                 'key': 'value',
+                                 'the answer': 42}}
+        write(filepath, data)
+        data_read = read(filepath)
+        self.assertEquals(data, data_read)
+        os.remove(filepath)  # cleanup of mkstemp
 
     def test_read_h5(self):
         source = pkg_resources.resource_filename('mpu', 'io.py')
