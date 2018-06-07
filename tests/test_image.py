abdf6e52cb8e2ae7cf97cbe8856136024530753f
Martin Thoma <info@martin-thoma.de>
2018-06-07 23:00:58 +0200

    TST: Add test for image.get_meta

---
+++ tests/test_image.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# core modules
+import unittest
+import pkg_resources
+
+# internal modules
+from mpu.image import get_meta
+
+
+class ImageTests(unittest.TestCase):
+
+    def test_get_meta(self):
+        path = '../tests/files/example.png'
+        source = pkg_resources.resource_filename('mpu', path)
+        meta = get_meta(source)
+        self.assertDictEqual(meta, {'width': 252, 'height': 167})
