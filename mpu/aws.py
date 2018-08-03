2c6c653b199cc48d411d0a53ffd9742d2ea1b77d
Martin Thoma <info@martin-thoma.de>
2018-08-03 22:47:29 +0200

    DOC: aws module can raise a lot of exceptions
    See #36

--- mpu/aws.py
+++ mpu/aws.py
@@ -47,6 +47,14 @@ def s3_download(source, destination, profile_name=None):
     destination : str
     profile_name : str, optional
         AWS profile
+
+    Raises
+    ------
+    botocore.exceptions.NoCredentialsError
+        Botocore is not able to find your credentials. Either specify
+        profile_name or add the environment variables AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY and AWS_SESSION_TOKEN.
+        See https://boto3.readthedocs.io/en/latest/guide/configuration.html
     """
     session = boto3.Session(profile_name=profile_name)
     s3 = session.resource('s3')
