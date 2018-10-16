77698e59c2254c3710e12f75ed20d8e4b3eb55d1
Martin Thoma <info@martin-thoma.de>
2018-10-16 21:17:07 +0200

    DEV: Add tests_require

--- setup.py
+++ setup.py
@@ -30,8 +30,9 @@ requires_datetime = ['pytz']
 requires_image = ['Pillow']
 requires_io = ['pytz', 'tzlocal']
 requires_aws = ['boto3']
+requires_tests = ['pytest', 'pytest-cov', 'pytest-mccabe', 'pytest-flake8']
 requires_all = (['pandas', 'python-magic'] + requires_datetime +
-                requires_image + requires_io + requires_aws)
+                requires_image + requires_io + requires_aws + requires_tests)
 
 config = {
     'name': 'mpu',
@@ -48,7 +49,9 @@ config = {
                        'aws': requires_aws,
                        'datetime': requires_datetime,
                        'image': requires_image,
-                       'io': requires_io},
+                       'io': requires_io,
+                       'tests': requires_tests},
+    'tests_require': requires_tests,
     'entry_points': {
         'console_scripts': ['mpu=mpu._cli:main']
     },
