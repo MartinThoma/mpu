92d760325dfd7191826053679baf6b8bb878ea0a
Martin Thoma <info@martin-thoma.de>
2019-10-19 22:25:23 +0200

    MAINT: Improvements found by mutmut

--- mpu/string.py
+++ mpu/string.py
@@ -47,9 +47,11 @@ def is_email(potential_email_address):
     False
     >>> is_email('info@martin-thoma')
     False
+    >>> is_email('Martin <>')
+    False
     """
     context, mail = parseaddr(potential_email_address)
-    first_condition = len(context) == 0 and len(mail) != 0
+    first_condition = len(context) == 0
     dot_after_at = ('@' in potential_email_address
                     and '.' in potential_email_address.split('@')[1])
     return first_condition and dot_after_at
