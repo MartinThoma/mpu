9e76f14e8ca1174760949d0edbb60213d071bacf
Martin Thoma <info@martin-thoma.de>
2020-05-01 17:50:03 +0200

    ENH: Add geometry

---
+++ mpu/geometry.py
@@ -0,0 +1,298 @@
+"""
+Create and manipulate two-dimensional geometrical entities such as lines.
+
+For more advanced use cases, see:
+
+* sympy.geometry
+* [Shapely](https://pypi.org/project/Shapely/)
+"""
+
+# Core Library
+import math
+from typing import FrozenSet, List, Set, Tuple, Union
+
+# First party
+from mpu.datastructures import Interval
+
+EPSILON = 0.000001
+
+
+class Point:
+    """
+    A point in a 2-dimensional Euclidean space.
+
+    Parameters
+    ----------
+    x : float
+    y : float
+    """
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}|{self.y})"
+
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class LineSegment:
+    """
+    A line segment a a 2-dimensional Euclidean space.
+
+    Parameters
+    ----------
+    p1 : Point
+    p2 : Point
+    """
+
+    def __init__(self, p1: Point, p2: Point, name: str = "LineSegment"):
+        self.p1 = p1
+        self.p2 = p2
+        self.name = name
+
+    def length(self) -> float:
+        """Get the length of this line segment."""
+        return ((self.p1.x - self.p2.x) ** 2 + (self.p1.y - self.p2.y) ** 2) ** 0.5
+
+    def is_point(self):
+        """Check if this LineSegment is a point."""
+        return self.p1 == self.p2
+
+    def angle(self) -> float:
+        """Get the angle of this line."""
+        xf, yf = self.p1.x, self.p1.y
+        xs, ys = self.p2.x, self.p2.y
+
+        min_x = min(xf, xs)
+        min_y = min(yf, ys)
+
+        x = max(xf - min_x, xs - min_x)
+        y = max(yf - min_y, ys - min_y)
+
+        return math.atan2(y, x) / math.pi * 180 % 360
+
+    def _get_equation_parameters(self):
+        if self.p1.x == self.p2.x:
+            raise ValueError("is not a function")
+        # y1 = m*x1 + t
+        # y2 = m*x2 + t
+        # => y1 = m*x1 + (y2-m*x2)
+        # <=> m = (y1 - y2) /(x1-x2)
+        #    t = y1 - m*x1
+        y1 = self.p1.y
+        y2 = self.p2.y
+        x1 = self.p1.x
+        x2 = self.p2.x
+        m = (y1 - y2) / (x1 - x2)
+        t = y1 - m * x1
+        return m, t
+
+    def simplify(self):
+        """Simplify this line segment to a point, if possible."""
+        if self.is_point():
+            return self.p1
+        if self.p1.x > self.p2.x:
+            return LineSegment(p1=self.p2, p2=self.p1)
+        else:
+            return self
+
+    def intersect(self, other) -> Union[None, "LineSegment", Point]:
+        """
+        Get the intersection between this LineSegment and another LineSegment.
+
+        Parameters
+        ----------
+        other : LineSegment
+
+        Returns
+        -------
+        intersection : Union[None, LineSegment, Point]
+        """
+        if not do_lines_intersect(self, other):
+            return None
+        if self.is_point():
+            p1 = self.simplify()
+            return p1  # we know they intersect
+        elif other.is_point():
+            return other.intersect(self)
+        elif self.angle() == other.angle():
+            # The overlap is a line segment or a point!
+            if self.angle() in [90, 270]:
+                # The line segment is not a function
+                x = self.p1.x
+
+                assert x == other.p1.x
+                return _get_straight_line_intersection(
+                    x, other.p1.y, other.p2.y, self.p1.y, self.p2.y
+                )
+            else:
+                # The LineSegment is a function
+                x_start = max(min(self.p1.x, self.p2.x), min(other.p1.x, other.p2.x))
+                x_end = min(max(self.p1.x, self.p2.x), max(other.p1.x, other.p2.x))
+                m, t = self._get_equation_parameters()
+                p1 = Point(x_start, m * x_start + t)
+                p2 = Point(x_end, m * x_end + t)
+                return LineSegment(p1, p2)
+        else:
+            # We know that we have to real line segments, that those intersect
+            # and that their angle is different. Hence the return value
+            # must be a point
+            if self.angle() in [90, 270]:
+                x = self.p1.x
+
+                if other.angle() in [90, 270]:
+                    assert x == other.p1.x
+                    return _get_straight_line_intersection(
+                        x, other.p1.y, other.p2.y, self.p1.y, self.p2.y
+                    )
+                else:
+                    m, t = other._get_equation_parameters()
+                    y = m * x + t
+                    return Point(x, y)
+            elif other.angle() in [90, 270]:
+                x = other.p1.x
+                m, t = self._get_equation_parameters()
+                y = m * x + t
+                return Point(x, y)
+            else:
+                # The overlap is a point
+                m1, t1 = self._get_equation_parameters()
+                m2, t2 = other._get_equation_parameters()
+                # m1 * x + t1 = m2 * x + t2
+                # <=> (m1 - m2) * x = t2 - t1
+                # <=> x = (t2 - t1) / (m1 - m2)
+                x = (t2 - t1) / (m1 - m2)
+                y = m1 * x + t1
+                return Point(x, y)
+
+    def bounding_box(self) -> Tuple[Point, Point]:
+        """
+        Get the bounding box of this line represented by two points.
+
+        The p1 point is in the lower left corner, the p2 one at the
+        upper right corner.
+        """
+        result = (
+            Point(min(self.p1.x, self.p2.x), min(self.p1.y, self.p2.y)),
+            Point(max(self.p1.x, self.p2.x), max(self.p1.y, self.p2.y)),
+        )
+        return result
+
+    def __str__(self) -> str:
+        if self.name == "LineSegment":
+            return f"LineSegment [{self.p1} to {self.p2}]"
+        else:
+            return self.name
+
+    __repr__ = __str__
+
+    def __hash__(self):
+        return hash((self.p1, self.p2, self.name))
+
+    def __eq__(self, other):
+        if not isinstance(other, LineSegment):
+            return False
+        return self.name == other.name and (
+            (self.p1 == other.p1 and self.p2 == other.p2)
+            or (self.p1 == other.p2 and self.p2 == other.p1)
+        )
+
+
+def _get_straight_line_intersection(x, other_y1, other_y2, self_y1, self_y2):
+    """Get the intersection point of two straight vertical lines."""
+    self_y = Interval(left=min(self_y1, self_y2), right=max(self_y1, self_y2))
+    other_y = Interval(left=min(other_y1, other_y2), right=max(other_y1, other_y2))
+
+    intersection = self_y.intersection(other_y)
+    if intersection.left == intersection.right:
+        return Point(x, intersection.left)
+    else:
+        return LineSegment(Point(x, intersection.left), Point(x, intersection.right))
+
+
+def do_bounding_boxes_intersect(a: Tuple[Point, Point], b: Tuple[Point, Point]) -> bool:
+    """
+    Check if bounding boxes do intersect.
+
+    If one bounding box touches the other, they do intersect.
+    """
+    return (
+        a[0].x <= b[1].x and a[1].x >= b[0].x and a[0].y <= b[1].y and a[1].y >= b[0].y
+    )
+
+
+def crossproduct(a: Point, b: Point) -> float:
+    """Get the cross product of two points."""
+    return a.x * b.y - b.x * a.y
+
+
+def is_point_on_line(a: LineSegment, b: Point) -> bool:
+    """Check if point b is on LineSegment a."""
+    # Move the image, so that a.p1 is on (0|0)
+    p2 = Point(a.p2.x - a.p1.x, a.p2.y - a.p1.y)
+    a_tmp = LineSegment(Point(0, 0), p2)
+    b_tmp = Point(b.x - a.p1.x, b.y - a.p1.y)
+    r = crossproduct(a_tmp.p2, b_tmp)
+    return abs(r) < EPSILON
+
+
+def is_point_right_of_line(a: LineSegment, b: Point) -> bool:
+    """Check if point b is right of line a."""
+    # Move the image, so that a.p1 is on (0|0)
+    a_tmp = LineSegment(Point(0, 0), Point(a.p2.x - a.p1.x, a.p2.y - a.p1.y))
+    b_tmp = Point(b.x - a.p1.x, b.y - a.p1.y)
+    return crossproduct(a_tmp.p2, b_tmp) < 0
+
+
+def line_segment_touches_or_crosses_line(a: LineSegment, b: LineSegment) -> bool:
+    """Check if line segment a touches or crosses line segment b."""
+    return (
+        is_point_on_line(a, b.p1)
+        or is_point_on_line(a, b.p2)
+        or (is_point_right_of_line(a, b.p1) ^ is_point_right_of_line(a, b.p2))
+    )
+
+
+def do_lines_intersect(a: LineSegment, b: LineSegment) -> bool:
+    """Check if LineSegments a and b intersect."""
+    box1 = a.bounding_box()
+    box2 = b.bounding_box()
+    return (
+        do_bounding_boxes_intersect(box1, box2)
+        and line_segment_touches_or_crosses_line(a, b)
+        and line_segment_touches_or_crosses_line(b, a)
+    )
+
+
+def get_all_intersecting_lines_by_brute_force(
+    lines: List[LineSegment],
+) -> Set[FrozenSet[LineSegment]]:
+    """
+    Get all interectionLines by applying a brute force algorithm.
+
+    Parameters
+    ----------
+    lines : all lines you want to check, in no order
+
+    Returns
+    -------
+    a list that contains all pairs of intersecting lines
+    """
+    intersections: Set[FrozenSet[LineSegment]] = set()
+
+    for i in range(len(lines)):
+        for j in range(i + 1, len(lines)):
+            if do_lines_intersect(lines[i], lines[j]):
+                tmp = frozenset({lines[i], lines[j]})
+                intersections.add(tmp)
+    return intersections
---
+++ tests/test_geometry.py
@@ -0,0 +1,375 @@
+# Core Library
+import random
+from typing import List, Set
+
+# Third party
+import pytest
+
+# First party
+from mpu.geometry import (
+    EPSILON,
+    LineSegment,
+    Point,
+    _get_straight_line_intersection,
+    crossproduct,
+    do_bounding_boxes_intersect,
+    do_lines_intersect,
+    get_all_intersecting_lines_by_brute_force,
+    is_point_right_of_line,
+    line_segment_touches_or_crosses_line,
+)
+
+
+def test_antisymmetry_of_cross_product():
+    points = [Point(0, 0), Point(1, 1)]
+    for i in range(50):
+        points.append(Point(random.random(), random.random()))
+
+    for p1 in points:
+        for p2 in points:
+            r1 = crossproduct(p1, p2)
+            r2 = crossproduct(p2, p1)
+            assert abs(r1 + r2) < EPSILON, f"[{p1}, {p2}]"
+
+
+def test_point_equality():
+    p1 = Point(0, 0)
+    assert not (p1 == "(0|0)")
+
+    p2 = Point(0.0, 0.0)
+    assert p1 == p2
+
+
+def test_line_segment_simplify():
+    line_segment1 = LineSegment(Point(42, 0), Point(0, 0))
+    assert str(line_segment1.simplify()) == "LineSegment [(0|0) to (42|0)]"
+
+
+def test_line_segment_equality():
+    line_segment1 = LineSegment(Point(0, 0), Point(42, 0))
+    line_segment2 = LineSegment(Point(42, 0), Point(0, 0))
+    assert line_segment1 == line_segment2
+
+    line_segment1 = LineSegment(Point(0, 0), Point(42.1, 0))
+    line_segment2 = LineSegment(Point(42, 0), Point(0, 0))
+    assert line_segment1 != line_segment2
+
+    assert line_segment1 != "[(0|0), (42|0)]"
+
+
+def test_line_segment_no_intersection1():
+    """No intersection"""
+    ls1 = LineSegment(Point(0, 0), Point(1, 0))
+    ls2 = LineSegment(Point(2, 0), Point(3, 0))
+    assert ls1.intersect(ls2) is None
+    assert ls2.intersect(ls1) is None
+
+
+def test_line_segment_no_intersection2():
+    """No intersection"""
+    ls1 = LineSegment(Point(0, 0), Point(0, 1))
+    ls2 = LineSegment(Point(0, 2), Point(0, 3))
+    assert ls1.intersect(ls2) is None
+    assert ls2.intersect(ls1) is None
+
+
+def test_line_segment_point_intersection():
+    """A single point intersection"""
+    ls1 = LineSegment(Point(0, 0), Point(0, 1))
+    ls2 = LineSegment(Point(0, 1), Point(0, 3))
+    assert ls1.intersect(ls2) == Point(0, 1)
+    assert ls2.intersect(ls1) == Point(0, 1)
+
+
+def test_line_segment_point_intersection1():
+    """A single point intersection"""
+    ls1 = LineSegment(Point(0, 0), Point(1, 1))
+    ls2 = LineSegment(Point(1, 1), Point(3, 3))
+    assert ls1.intersect(ls2).simplify() == Point(1, 1)
+    assert ls2.intersect(ls1).simplify() == Point(1, 1)
+
+
+def test_line_segment_point_intersection2():
+    """A single point intersection"""
+    ls1 = LineSegment(Point(0, 0), Point(1, 1))
+    ls2 = LineSegment(Point(1, 1), Point(1, 1))
+    assert ls1.intersect(ls2) == Point(1, 1)
+    assert ls2.intersect(ls1) == Point(1, 1)
+
+
+def test_line_segment_point_intersection3():
+    """A single point intersection"""
+    ls1 = LineSegment(Point(5, 0), Point(5, 10))
+    ls2 = LineSegment(Point(0, 0), Point(20, 20))
+    assert ls1.intersect(ls2) == Point(5, 5)
+    assert ls2.intersect(ls1) == Point(5, 5)
+
+
+def test_line_segment_point_intersection4():
+    """A single point intersection"""
+    ls1 = LineSegment(Point(0, 4), Point(20, 4))
+    ls2 = LineSegment(Point(0, 0), Point(20, 20))
+    assert ls1.intersect(ls2) == Point(4, 4)
+    assert ls2.intersect(ls1) == Point(4, 4)
+
+
+def test_line_segment_length():
+    line_segment = LineSegment(Point(0, 0), Point(42, 0))
+    assert line_segment.length() == 42
+
+    line_segment = LineSegment(Point(0, 0), Point(3, 4))
+    assert line_segment.length() == 5
+
+
+def test_line_segment_angle():
+    line_segment = LineSegment(Point(0, 0), Point(1, 0))
+    assert line_segment.angle() == 0
+
+    line_segment = LineSegment(Point(0, 0), Point(-1, 0))
+    assert line_segment.angle() == 0
+
+
+def test_get_equation_parameters():
+    line_segment = LineSegment(Point(0, 0), Point(1, 0))
+    assert line_segment._get_equation_parameters() == (0, 0)
+
+    line_segment = LineSegment(Point(0, 0), Point(1, 1))
+    assert line_segment._get_equation_parameters() == (1, 0)
+
+    line_segment = LineSegment(Point(0, 0), Point(0, 1))
+    with pytest.raises(ValueError):
+        line_segment._get_equation_parameters()
+
+
+def test_point_right_of_line():
+    line = LineSegment(Point(0, 0), Point(0, 7))
+    a = Point(5, 5)
+    assert is_point_right_of_line(line, a)
+
+
+def test_point_left_of_line():
+    line = LineSegment(Point(0, 0), Point(0, 7))
+    a = Point(-5, 5)
+    assert not is_point_right_of_line(line, a)
+
+
+def test_point_on_line():
+    line = LineSegment(Point(0, 0), Point(4, 4))
+    a = Point(3, 3)
+    assert not is_point_right_of_line(line, a)
+
+    line = LineSegment(Point(4, 4), Point(0, 0))
+    assert not is_point_right_of_line(line, a)
+
+
+def test_bounding_boxes_intersect_t1():
+    a = [Point(0, 0), Point(5, 5)]
+    b = [Point(1, 1), Point(2, 2)]
+    assert do_bounding_boxes_intersect(a, b)
+
+
+def test_bounding_boxes_intersect_t2():
+    a = [Point(0, 0), Point(3, 3)]
+    b = [Point(1, -1), Point(2, 7)]
+    assert do_bounding_boxes_intersect(a, b)
+
+
+def test_bounding_boxes_intersect_t3():
+    a = [Point(0, 0), Point(3, 3)]
+    b = [Point(1, -1), Point(2, 2)]
+    assert do_bounding_boxes_intersect(a, b)
+
+
+def test_bounding_boxes_intersect_t4():
+    a = [Point(0, 0), Point(3, 3)]
+    b = [Point(3, 3), Point(5, 5)]
+    assert do_bounding_boxes_intersect(a, b)
+
+
+def test_bounding_boxes_intersect_f1():
+    a = [Point(0, 0), Point(3, 3)]
+    b = [Point(4, 4), Point(5, 5)]
+    assert not do_bounding_boxes_intersect(a, b)
+
+
+def test_line_segment_corsses_line():
+    """Tests for lineSegmentCrossesLine"""
+    line_segment = LineSegment(Point(5, 5), Point(17, 17))
+    line = LineSegment(Point(0, 0), Point(1, 1))
+    assert line_segment_touches_or_crosses_line(line_segment, line)
+
+    line_segment = LineSegment(Point(17, 17), Point(5, 5))
+    line = LineSegment(Point(0, 0), Point(1, 1))
+    assert line_segment_touches_or_crosses_line(line_segment, line)
+
+
+def test_lines_dont_intesect_f1():
+    """Tests for do_lines_intersect"""
+    a = LineSegment(Point(0, 0), Point(7, 7))
+    b = LineSegment(Point(3, 4), Point(4, 5))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_dont_intesect_f2():
+    a = LineSegment(Point(-4, 4), Point(-2, 1))
+    b = LineSegment(Point(-2, 3), Point(0, 0))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_dont_intesect_f3():
+    a = LineSegment(Point(0, 0), Point(0, 1))
+    b = LineSegment(Point(2, 2), Point(2, 3))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_dont_intesect_f4():
+    a = LineSegment(Point(0, 0), Point(0, 1))
+    b = LineSegment(Point(2, 2), Point(3, 2))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_dont_intesect_f5():
+    a = LineSegment(Point(-1, -1), Point(2, 2))
+    b = LineSegment(Point(3, 3), Point(5, 5))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_dont_intesect_f6():
+    a = LineSegment(Point(0, 0), Point(1, 1))
+    b = LineSegment(Point(2, 0), Point(0.5, 2))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_dont_intesect_f7():
+    a = LineSegment(Point(1, 1), Point(4, 1))
+    b = LineSegment(Point(2, 2), Point(3, 2))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_dont_intesect_f8():
+    a = LineSegment(Point(0, 5), Point(6, 0))
+    b = LineSegment(Point(2, 1), Point(2, 2))
+    assert not do_lines_intersect(a, b)
+
+
+def test_lines_do_intesect_t1():
+    a = LineSegment(Point(0, -2), Point(0, 2))
+    b = LineSegment(Point(-2, 0), Point(2, 0))
+    assert do_lines_intersect(a, b)
+
+
+def test_lines_do_intesect_t2():
+    a = LineSegment(Point(5, 5), Point(0, 0))
+    b = LineSegment(Point(1, 1), Point(8, 2))
+    assert do_lines_intersect(a, b)
+
+
+def test_lines_do_intesect_t3():
+    a = LineSegment(Point(-1, 0), Point(0, 0))
+    b = LineSegment(Point(-1, -1), Point(-1, 1))
+    assert do_lines_intersect(a, b)
+
+
+def test_lines_do_intesect_t4():
+    a = LineSegment(Point(0, 2), Point(2, 2))
+    b = LineSegment(Point(2, 0), Point(2, 4))
+    assert do_lines_intersect(a, b)
+
+
+def test_lines_do_intesect_t5():
+    a = LineSegment(Point(0, 0), Point(5, 5))
+    b = LineSegment(Point(1, 1), Point(3, 3))
+    assert do_lines_intersect(a, b)
+
+
+def test_lines_do_intesect_t6():
+    for i in range(50):
+        ax = random.random()
+        ay = random.random()
+        bx = random.random()
+        by = random.random()
+        a = LineSegment(Point(ax, ay), Point(bx, by))
+        b = LineSegment(Point(ax, ay), Point(bx, by))
+        assert do_lines_intersect(a, b)
+
+
+def test_blog_example():
+    """check getAllIntersectingLines()"""
+    lines: List[LineSegment] = [
+        LineSegment(Point(1, 4), Point(6, 1), "a"),
+        LineSegment(Point(2, 1), Point(5, 4), "b"),
+        LineSegment(Point(3, 1), Point(6, 4), "c"),
+        LineSegment(Point(4, 1), Point(8, 5), "d"),
+        LineSegment(Point(3, 4), Point(9, 3), "e"),
+        LineSegment(Point(7, 2), Point(9, 3), "f"),
+        LineSegment(Point(6, 7), Point(9, 1), "g"),
+        LineSegment(Point(11, 1), Point(16, 5), "h"),
+        LineSegment(Point(13, 3), Point(13, 4), "i"),
+        LineSegment(Point(15, 3), Point(15, 4), "j"),
+        LineSegment(Point(13, 2), Point(14, 2), "k"),
+        LineSegment(Point(14, 1), Point(14, 2), "l"),
+        LineSegment(Point(17, 3), Point(21, 3), "m"),
+        LineSegment(Point(19, 5), Point(19, 1), "n"),
+        LineSegment(Point(11, 1), Point(16, 5), "o"),
+    ]
+
+    intersections: Set[LineSegment] = set()
+    add = frozenset({lines[0], lines[1]})
+    intersections.add(add)
+    add = frozenset({lines[0], lines[2]})
+    intersections.add(add)
+    add = frozenset({lines[0], lines[3]})
+    intersections.add(add)
+    add = frozenset({lines[3], lines[6]})
+    intersections.add(add)
+    add = frozenset({lines[4], lines[1]})
+    intersections.add(add)
+    add = frozenset({lines[4], lines[2]})
+    intersections.add(add)
+    add = frozenset({lines[4], lines[3]})
+    intersections.add(add)
+    add = frozenset({lines[4], lines[5]})
+    intersections.add(add)
+    add = frozenset({lines[4], lines[6]})
+    intersections.add(add)
+    add = frozenset({lines[5], lines[6]})
+    intersections.add(add)
+    add = frozenset({lines[10], lines[11]})
+    intersections.add(add)
+    add = frozenset({lines[12], lines[13]})
+    intersections.add(add)
+    add = frozenset({lines[7], lines[14]})
+    intersections.add(add)
+
+    intersectionsBrute: Set[LineSegment] = get_all_intersecting_lines_by_brute_force(
+        lines
+    )
+    # intersectionsSweep: Set[LineSegment] = getAllIntersectingLines(lines)
+
+    assert intersectionsBrute == intersections
+
+    # twice [e,g], but not [[d, g], [e, c], [e, d], [e, f]]
+    # assert intersectionsSweep == intersections
+
+
+def test_compare_to_brute_force():
+    n = 30
+    max_intersections = (n * n - n) / 2
+
+    lines: List[LineSegment] = []
+    for i in range(n):
+        a = Point(random.random(), random.random())
+        b = Point(random.random(), random.random())
+        lines.append(LineSegment(a, b))
+
+    intersectionsBrute: Set[LineSegment] = get_all_intersecting_lines_by_brute_force(
+        lines
+    )
+    # intersectionsSweep: Set[LineSegment] = getAllIntersectingLines(lines)
+    assert len(intersectionsBrute) <= max_intersections
+    # assert intersectionsBrute == intersectionsSweep
+
+
+def test_get_straight_line_intersection():
+    intersection = _get_straight_line_intersection(0, 0, 10, 5, 20)
+    assert intersection == LineSegment(Point(0, 5), Point(0, 10))
